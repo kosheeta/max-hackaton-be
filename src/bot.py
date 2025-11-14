6deac04a402885b736dcbfd744e751c8d924abc9
Darkness <inflexibled@gmail.com>
2025-11-14 22:01:29 +0300

    test

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ import json
 from maxapi import Bot, Dispatcher
 from maxapi.enums.parse_mode import ParseMode
 from maxapi.enums.upload_type import UploadType
-from maxapi.types import Attachment, PhotoAttachmentPayload
+from maxapi.types import Attachment, OtherAttachmentPayload
 from pydantic import BaseModel
 from rewire import config, simple_plugin, DependenciesModule, logger
 
@@ -41,15 +41,14 @@ async def delete_user_message(message_id: str):
     await get_bot().delete_message(message_id)
 
 
-async def upload_image(file_path: str) -> PhotoAttachmentPayload:
+async def upload_image(file_path: str) -> OtherAttachmentPayload:
     upload_url = await get_bot().get_upload_url(UploadType.IMAGE)
     upload_result = await get_bot().upload_file(upload_url.url, file_path, UploadType.IMAGE)
 
     photos_data = json.loads(upload_result)['photos']
-    photo_id, photo_data = next(iter(photos_data.items()))
+    photo_data = next(iter(photos_data.values()))
 
-    return PhotoAttachmentPayload(
-        photo_id=photo_id,
+    return OtherAttachmentPayload(
         token=photo_data['token'],
         url=upload_url.url
     )
